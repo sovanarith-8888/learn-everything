2a430b732847d11e5ef636401097c748b1c5c649
sovannarith chin <sovannarith.chin@bslandhome.com>
2024-04-30 17:10:08 +0700

    table render

--- app/(admin)/dashboard/page.tsx
+++ app/(admin)/dashboard/page.tsx
@@ -1,7 +1,39 @@
-
+"use client"
+import DataTable from 'react-data-table-component';
 const page = () => {
+  const columns = [
+    {
+      name: 'Title',
+      selector: row => row.title,
+      sortable: true,
+    },
+    {
+      name: 'Year',
+      selector: row => row.year,
+      sortable: true,
+    },
+  ];
+  
+  const data = [
+      {
+      id: 1,
+      title: 'Beetlejuice',
+      year: '1988',
+    },
+    {
+      id: 2,
+      title: 'Ghostbusters',
+      year: '1984',
+    },
+  ]
+  
   return (
-    <h1>MY Dashboard</h1>
+    <>
+      <DataTable
+			  columns={columns}
+			  data={data}
+		  />
+    </>
   )
 }
 
